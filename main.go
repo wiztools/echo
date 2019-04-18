35d7fdfa0e81a1bb131faa718608e64093dc910e
Subhash Chandran <subwiz@gmail.com>
2019-04-19 00:47:25 +0530

    unnecessary builder removed

--- main.go
+++ main.go
@@ -5,21 +5,18 @@ import (
 	"bytes"
 	"fmt"
 	"net/http"
-	"strings"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		var str strings.Builder
 
 		// Request:
 		var b bytes.Buffer
 		wrtr := bufio.NewWriter(&b)
 		r.Write(wrtr)
 		wrtr.Flush()
-		str.WriteString(string(b.Bytes()))
 
-		fmt.Fprintf(w, str.String())
+		fmt.Fprintf(w, string(b.Bytes()))
 	})
 	http.ListenAndServe(":3000", nil)
 }
